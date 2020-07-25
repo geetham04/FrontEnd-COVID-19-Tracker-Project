029902c5174cd2796f237deaba569893470ee504
unknown <geetham@pdx.edu>
2020-07-25 16:31:16 -0700

    developed countrypicker functionality

--- coronareactapp/src/components/CountryPicker/CountryPicker.jsx
+++ coronareactapp/src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import styles from './CountryPicker.module.css';
+import { fetchCountries } from '../../api';
 
 // functional component
 const CountryPicker = () => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchCountriesAPI = async () => {
+            
+            setFetchedCountries(await fetchCountries());
+        }
+        fetchCountriesAPI();
+    }, [setFetchedCountries]);
+
+    // console.log(fetchedCountries);
+
     return (
-        <h1>CountryPicker</h1>
+        <FormControl className={styles.FormControl}>
+            <NativeSelect>
+                <option value="global">Global</option>
+                {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
