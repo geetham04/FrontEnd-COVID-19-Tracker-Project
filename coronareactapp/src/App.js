985d5f3a7f233aa271f697e70147b20b6d697dc7
unknown <geetham@pdx.edu>
2020-08-01 15:15:35 -0700

    code formatted

--- coronareactapp/src/App.js
+++ coronareactapp/src/App.js
@@ -37,7 +37,10 @@ class App extends React.Component {
         const { data, country } = this.state;
         return (
             <div className={styles.container}>
-                {hour>=12 ? hour>=16 ? <h1>Good Evening, Welcome to Coronavirus tracking application</h1> : <h1>Good Afternoon, Welcome to Coronavirus tracking application</h1> : <h1>Good Morning, Welcome to Coronavirus tracking application</h1>}
+                {
+                hour>=12 ? hour>=16 ? 
+                <h1>Good Evening, Welcome to Coronavirus tracking application</h1> : <h1>Good Afternoon, Welcome to Coronavirus tracking application</h1> : <h1>Good Morning, Welcome to Coronavirus tracking application</h1>
+                }
                 <img className={styles.image} src={covidImage} alt="COVID-19" />
                 <Cards data={data} />
                 <CountryPicker dealCountryChange={this.dealCountryChange} />
