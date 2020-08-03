15d5e42c280032bb5853528000d6459a8008eb94
unknown <geetham@pdx.edu>
2020-08-03 10:13:27 -0700

    Exported header and footer components

--- coronareactapp/src/components/index.js
+++ coronareactapp/src/components/index.js
@@ -2,3 +2,6 @@
 export { default as Cards } from './Cards/Cards';
 export { default as Chart } from './Chart/Chart';
 export { default as CountryPicker } from './CountryPicker/CountryPicker';
+export { default as Footer } from './footer';
+export { default as Header } from './header';
+
