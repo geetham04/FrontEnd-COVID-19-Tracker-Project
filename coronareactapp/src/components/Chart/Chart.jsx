aecebed1304d8c807a392f99a601a79a2df2b358
unknown <geetham@pdx.edu>
2020-08-06 17:46:02 -0700

    Added bar chart to visualize global daily data of infected cases

--- coronareactapp/src/components/Chart/Chart.jsx
+++ coronareactapp/src/components/Chart/Chart.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { fetchDailyData } from '../../api';
+import { fetchDailyData, fetchDailyDataPerDay } from '../../api';
 import { Line, Bar, Pie, Doughnut, Polar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
 // functional component
 const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     const [dailyData, setDailyData] = useState([]);
+    const [dailyDataPerDay, setDailyDataPerDay] = useState([]);
     useEffect(() => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
+            setDailyDataPerDay(await fetchDailyDataPerDay());
         }
-        
         fetchAPI();
     }, []);
+
 // eslint-disable-next-line no-unused-expressions
 
     const lineChart = (
@@ -34,8 +36,31 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
                     fill: true
                 }],
             }}
+            options={{
+                  title: { display: true, text:`Daily Cases Growth` },
+                }}
           />) : null
     );
+
+    const BarChart = (
+      dailyDataPerDay.length
+        ? (
+          <Bar
+          data={{
+              labels: dailyDataPerDay.map(({ date }) => date),
+              datasets: [{
+                  data: dailyDataPerDay.map(({ confirmed }) => confirmed),
+                  label: 'Globally Infected',
+                  borderColor: '#3333ff',
+                  backgroundColor: 'blue',
+                  fill: true
+              }]
+          }}
+          options={{
+                  title: { display: true, text:`Daily Infected Cases Timeline` },
+                }}
+        />) : null
+  );
 
     console.log(confirmed, recovered, deaths);
 
@@ -139,41 +164,38 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
            </div>
         ) : null
   )
-const polarChart = (
-  confirmed
-    ? (
-      // <div className="chart">
-        <Polar
-          data={{
-            labels: [
-              'Infected',
-              'Recovered', 
-              'Deaths'
-              ],
-            datasets: [{
-              label: 'people',
-              backgroundColor: [
-                 'rgba(0, 0, 255, 0.7)',
-                 'rgba(0, 96, 96, 0.7)',
-                 'rgba(255, 0, 0, 0.7)'
-              ],
-              data:[
-                confirmed.value, 
-                recovered.value, 
-                deaths.value
-                ]  
-            }]
-          }}
-          width={"30%"}
-          // height={50}
-          options={{
-            maintainAspectRatio: false,
-            legend: { display: false }, 
-            title: { display: true, text:`Currently showing ${country} country` }
-          }}
-       />
-      //  </div>
-    ) : null
+  const polarChart = (
+    confirmed
+      ? (
+        <div id="chart-wrapper">
+          <Polar
+            data={{
+              labels: [
+                'Infected',
+                'Recovered', 
+                'Deaths'
+                ],
+              datasets: [{
+                label: 'people',
+                backgroundColor: [
+                   'rgba(0, 0, 255, 0.7)',
+                   'rgba(0, 96, 96, 0.7)',
+                   'rgba(255, 0, 0, 0.7)'
+                ],
+                data:[
+                  confirmed.value, 
+                  recovered.value, 
+                  deaths.value
+                  ]  
+              }]
+            }}
+            options={{
+              legend: { display: false }, 
+              title: { display: true, text:`${country} country data in polar chart` },
+            }}
+         />
+         </div>
+      ) : null
 )
 
 const columnChart = (
@@ -212,7 +234,7 @@ const columnChart = (
 
     return (
         <div className={styles.container}>
-          {country ? [barChart, pieChart, doughnutChart] : lineChart}
+          {country ? [barChart, pieChart, doughnutChart, polarChart] : [lineChart, BarChart]}
         </div>       
     )
   }
