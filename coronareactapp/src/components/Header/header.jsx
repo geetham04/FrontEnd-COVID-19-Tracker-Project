a2c2ed4c0b8243cff13f61912e2ad32c1f5a8d13
geetham04 <56003084+geetham04@users.noreply.github.com>
2020-08-09 16:11:20 -0700

    Update header.jsx

--- coronareactapp/src/components/Header/header.jsx
+++ coronareactapp/src/components/Header/header.jsx
@@ -1,13 +1,15 @@
+// importing libraries
 import React from "react";
 import Timer from '../Timer/timer';
 import styles from './header.module.css';
 //import HighlightIcon from "@material-ui/icons/Highlight";
 
+// Header() function return current time within a styled container
 function Header() {
   return (
     
     <nav className={styles.container}> 
-    <header class="redtopbanner text-right mx-xl-5 d-flex align-items-center justify-content-end">
+    <header class="topbanner text-right mx-xl-5 d-flex align-items-center justify-content-end">
       <h3>
       <Timer />
       </h3>
