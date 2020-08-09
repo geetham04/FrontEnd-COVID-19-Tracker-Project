9112eec8ce11aef3955f70d0477b04636178fb52
unknown <geetham@pdx.edu>
2020-08-09 11:18:09 -0700

    Updates

---
+++ precache-manifest.7f9e2ad9a0d4864d0a7b69daf069ce8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f785f3f1250da8f93fe7e56e5a1173",
+    "url": "/FrontEnd-CoronaVirus-Dashboard-Application/index.html"
+  },
+  {
+    "revision": "f0d32b7ca8eb6bc29630",
+    "url": "/FrontEnd-CoronaVirus-Dashboard-Application/static/css/main.18dd72ae.chunk.css"
+  },
+  {
+    "revision": "e8a4b1a56f6b7f5f0728",
+    "url": "/FrontEnd-CoronaVirus-Dashboard-Application/static/js/2.9ade449f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/FrontEnd-CoronaVirus-Dashboard-Application/static/js/2.9ade449f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d32b7ca8eb6bc29630",
+    "url": "/FrontEnd-CoronaVirus-Dashboard-Application/static/js/main.196d493e.chunk.js"
+  },
+  {
+    "revision": "5968b7f79554fd801e9c",
+    "url": "/FrontEnd-CoronaVirus-Dashboard-Application/static/js/runtime-main.c21b2272.js"
+  },
+  {
+    "revision": "3e4031422a3714c72f3582d6def2b4af",
+    "url": "/FrontEnd-CoronaVirus-Dashboard-Application/static/media/covidlogo.3e403142.png"
+  }
+]);
